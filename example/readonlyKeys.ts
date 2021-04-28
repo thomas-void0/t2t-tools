3817d1e83b84cf1270972b801d1ce10b0b39f783
yangjingyu <yjy15680489038@163.com>
2021-04-28 22:40:49 +0800

    feat: 新增_Readonly

--- example/readonlyKeys.ts
+++ example/readonlyKeys.ts
@@ -1,5 +1,25 @@
+import { _IfEquals, _Pick } from "../src/my-type";
 /**
  * 获取readonly的类型
  * @example
- *
+ * 输入：
+ * type Example = { readonly foo: string; bar: number };
+ * type example = _Readonly<Example>
+ * 输出：
+ * type example = {
+ *   readonly foo: string;
+ *}
  */
+type _Readonly<T> = _Pick<
+  T,
+  {
+    [K in keyof T]: _IfEquals<
+      { [O in K]: T[K] },
+      { -readonly [O in K]: T[K] },
+      never,
+      K
+    >;
+  }[keyof T]
+>;
+type Example = { readonly foo: string; bar: number };
+type example = _Readonly<Example>;
