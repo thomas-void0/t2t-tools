0b30061d436cf91ca8c79af61a5025a62195f50c
yangjingyu <yjy15680489038@163.com>
2021-05-02 03:04:42 +0800

    feat: add _DeepReadonly

--- example/deepReadonly.ts
+++ example/deepReadonly.ts
@@ -19,4 +19,18 @@ import { _Primitive } from "../baseType";
  *      };
  *    }
  */
-//_DeepReadongly
+//_DeepReadonly
+export type DeepReadonly<T> = T extends ((...args: any[]) => any) | Primitive
+  ? T
+  : T extends _DeepReadonlyArray<infer U>
+  ? _DeepReadonlyArray<U>
+  : T extends _DeepReadonlyObject<infer V>
+  ? _DeepReadonlyObject<V>
+  : T;
+/** @private */
+// tslint:disable-next-line:class-name
+export interface _DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+/** @private */
+export type _DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
